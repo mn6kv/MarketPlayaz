3f664e538182e814b22149e495e83383d172d940
khovaev <khovaev@micord.ru>
2021-08-19 16:32:30 +0300

    settings added

---
+++ src/main/java/org/example/MainMarketPlayaz.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainMarketPlayaz {
+  public static void main(String[] args) {
+    SpringApplication.run(MainMarketPlayaz.class, args);
+  }
+}
