69988113a3fc866e0c2ce04103f2ee09f2b34f47
khovaev <khovaev@micord.ru>
2021-08-26 01:55:51 +0300

    name fix

--- src/main/java/org/example/services/OrderService.java
+++ src/main/java/org/example/services/OrderService.java
@@ -11,6 +11,6 @@ public interface OrderService {
   ResponseEntity<List<OrderDto>> getOrderByEmail(String buyersEmail);
   ResponseEntity<List<OrderDto>> getOrderByArticle(Long article);
   ResponseEntity<List<OrderDto>> getOrderByInterval(LocalDateTime startDate, LocalDateTime endDate);
-  ResponseEntity<OrderDto> putOrderWithArgs(String buyersEmail, LocalDateTime date, List<Long> products);
+  ResponseEntity<OrderDto> saveOrderWithArgs(String buyersEmail, LocalDateTime date, List<Long> products);
   ResponseEntity<List<OrderDto>> getAllOrders();
 }
