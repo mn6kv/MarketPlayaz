e94749e6912d338d6e8a865905429ab259ef18d1
khovaev <khovaev@micord.ru>
2021-08-26 01:55:28 +0300

    response fix

--- src/main/java/org/example/services/OrderServiceImpl.java
+++ src/main/java/org/example/services/OrderServiceImpl.java
@@ -31,10 +31,7 @@ public class OrderServiceImpl implements OrderService {
 
   @Override
   public ResponseEntity<List<OrderDto>> getOrderByEmail(String buyersEmail) {
-    List<Order> orders = orderRepository.findOrdersByBuyerEmail(buyersEmail);
-    return !orders.isEmpty()
-        ? new ResponseEntity<>(OrderDto.from(orders), HttpStatus.OK)
-        : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return ResponseEntity.ok(OrderDto.from(orderRepository.findOrdersByBuyerEmail(buyersEmail)));
   }
 
   @Override
@@ -42,22 +39,17 @@ public class OrderServiceImpl implements OrderService {
     List<Order> orders = orderRepository.findOrdersByProductsContains(
         productRepository.findProductByArticle(article)
             .orElseThrow(() -> new RuntimeException("Product by article not found")));
-    return !orders.isEmpty()
-        ? new ResponseEntity<>(OrderDto.from(orders), HttpStatus.OK)
-        : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return ResponseEntity.ok(OrderDto.from(orders));
   }
 
   @Override
   public ResponseEntity<List<OrderDto>> getOrderByInterval(LocalDateTime startDate,
                                                            LocalDateTime endDate) {
-    List<Order> orders = orderRepository.findOrdersByDateBetween(startDate, endDate);
-    return !orders.isEmpty()
-        ? new ResponseEntity<>(OrderDto.from(orders), HttpStatus.OK)
-        : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return ResponseEntity.ok(OrderDto.from(orderRepository.findOrdersByDateBetween(startDate, endDate)));
   }
 
   @Override
-  public ResponseEntity<OrderDto> putOrderWithArgs(String buyersEmail, LocalDateTime date,
+  public ResponseEntity<OrderDto> saveOrderWithArgs(String buyersEmail, LocalDateTime date,
                                                    List<Long> productsIds) {
     return new ResponseEntity<>(OrderDto.from(orderRepository.save(Order.builder()
         .buyerEmail(buyersEmail)
